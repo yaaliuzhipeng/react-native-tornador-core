4472ddfaf2b3443a47f83385b38592a9a3bc17ac
liuzhipeng <liuzhipeng@haxibiao.com>
2022-04-15 15:08:30 +0800

    添加shadow配置类型

--- core.d.ts
+++ core.d.ts
@@ -1,26 +1,32 @@
-declare const global:any;
+declare const global: any;
 declare namespace Tornador {
     export type Size = { width: number, height: number };
     export type Rect = Size;
+    export type ShadowOpts = {
+        shadowColor: string;
+        shadowOpacity: number;
+        shadowRadius: number;
+        shadowOffset: { width: number, height: number };
+    }
 }
-declare const font:(fontsize: number) => number;
-declare const ft:(v:number) => number;
-declare const fth:(v:number) => number;
-declare const pixel:(dp:number) => number;
+declare const font: (fontsize: number) => number;
+declare const ft: (v: number) => number;
+declare const fth: (v: number) => number;
+declare const pixel: (dp: number) => number;
 declare const di: (...args) => void;
 declare const dw: (...args) => void;
 declare const de: (...args) => void;
-declare const ww:number;
-declare const wh:number;
-declare const os:"ios" | "android" | "windows" | "macos" | "web";
-declare const select:(object:any) => any;
-declare const isAndroid:boolean;
-declare const isIos:boolean;
-declare const isMac:boolean;
-declare const isWeb:boolean;
-declare const isWin:boolean;
-declare const fxs:any;
+declare const ww: number;
+declare const wh: number;
+declare const os: "ios" | "android" | "windows" | "macos" | "web";
+declare const select: (object: any) => any;
+declare const isAndroid: boolean;
+declare const isIos: boolean;
+declare const isMac: boolean;
+declare const isWeb: boolean;
+declare const isWin: boolean;
+declare const fxs: any;
 
-type TypeKind = 'string'|'function'|'null'|'undefined'|'number';
-declare const isType:(target: any, type: TypeKind) => boolean;
-declare const isNil:(target:any) => boolean;
+type TypeKind = 'string' | 'function' | 'null' | 'undefined' | 'number';
+declare const isType: (target: any, type: TypeKind) => boolean;
+declare const isNil: (target: any) => boolean;
